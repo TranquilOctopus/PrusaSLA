b86ab1e4b06075f0bfa56a53afc4e2d3713054e5
Alexander Lejon <alexander.lejon@cag.se>
2026-09-18 13:27:16 +0200

    M0.9 (unbuilt, fix): Move SLA archive format registry to Biz layer
    - Move SlaArchiveFormat.hpp/cpp and SL1Format.cpp from libslic3r/Format to
      src/slic3r-shared/include/src/Slic3r/Biz/ResultExport/SLA/
    - Fix namespace from Slic3r::sla to Slic3r::Biz::PrintHost::Sla (matching
      neighbouring SL1.hpp/SL1.cpp)
    - Use fully qualified Slic3r::Biz::Slicing::Sla::FileDataType in interface
    - Include libslic3r/SLAResult.hpp explicitly where FileDataType is used
    - Update CMakeLists.txt in both libslic3r and slic3r-shared
    - Move test from tests/sla_print/sla_print_tests.cpp to
      src/slic3r-shared/test/Slic3r/Biz/ResultExport/SLA/SlaArchiveFormatTests.cpp
    - Remove test from sla_print_tests.cpp and its libslic3r/Format include
    - No change to SL1 output

--- tests/sla_print/sla_print_tests.cpp
+++ tests/sla_print/sla_print_tests.cpp
@@ -9,7 +9,6 @@
 #include <libslic3r/TriangleMeshSlicer.hpp>
 #include <libslic3r/SLA/SupportTreeMesher.hpp>
 #include <libslic3r/BranchingTree/PointCloud.hpp>
-#include <libslic3r/Format/SlaArchiveFormat.hpp>
 #include "Slic3r/Biz/Algorithms/BoundingBox.hpp"
 
 namespace BB = Slic3r::Biz::Algorithms::BoundingBox;
@@ -218,55 +217,4 @@ TEST_CASE("halfcone test", "[halfcone]") {
     namespace triangle_mesh = Biz::Algorithms::TriangleMesh;
     triangle_mesh::its_merge_vertices(m);
     its_write_obj(m, "Halfcone.obj");
-}
-
-TEST_CASE("SlaArchiveFormatRegistry returns SL1 format", "[SlaArchiveFormat]") {
-    auto& registry = sla::SlaArchiveFormatRegistry::instance();
-    
-    // Register formats (idempotent)
-    sla::register_sla_archive_formats();
-    
-    // Test SL1 format
-    auto sl1 = registry.get("SL1");
-    REQUIRE(sl1 != nullptr);
-    REQUIRE(sl1->name() == "SL1");
-    REQUIRE(sl1->file_data_type() == sla::FileDataType::sl1_png);
-    auto exts = sl1->extensions();
-    REQUIRE(std::find(exts.begin(), exts.end(), "sl1") != exts.end());
-    REQUIRE(std::find(exts.begin(), exts.end(), "sl1s") != exts.end());
-    REQUIRE(std::find(exts.begin(), exts.end(), "zip") != exts.end());
-    
-    // Test SL1_SVG format
-    auto sl1_svg = registry.get("SL1_SVG");
-    REQUIRE(sl1_svg != nullptr);
-    REQUIRE(sl1_svg->name() == "SL1_SVG");
-    REQUIRE(sl1_svg->file_data_type() == sla::FileDataType::sl1_svg);
-    exts = sl1_svg->extensions();
-    REQUIRE(std::find(exts.begin(), exts.end(), "sl1svg") != exts.end());
-    
-    // Test case-insensitive lookup
-    auto sl1_lower = registry.get("sl1");
-    REQUIRE(sl1_lower != nullptr);
-    REQUIRE(sl1_lower->name() == "SL1");
-    
-    // Test unknown format returns nullptr
-    auto unknown = registry.get("UNKNOWN");
-    REQUIRE(unknown == nullptr);
-    
-    // Test find by extension
-    auto by_ext = registry.find_by_extension("sl1");
-    REQUIRE(by_ext != nullptr);
-    REQUIRE(by_ext->name() == "SL1");
-    
-    auto by_ext_svg = registry.find_by_extension("sl1svg");
-    REQUIRE(by_ext_svg != nullptr);
-    REQUIRE(by_ext_svg->name() == "SL1_SVG");
-    
-    auto by_unknown_ext = registry.find_by_extension("xyz");
-    REQUIRE(by_unknown_ext == nullptr);
-    
-    // Test names list
-    auto names = registry.names();
-    REQUIRE(std::find(names.begin(), names.end(), "sl1") != names.end());
-    REQUIRE(std::find(names.begin(), names.end(), "sl1_svg") != names.end());
 }
